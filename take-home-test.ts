086306938e65ce6029874f9a411f77c10bf68373
Salman Shakeel <shakeels22@gmail.com>
2025-03-22 00:43:14 -0400

    initial commit with some bug fixes

---
+++ take-home-test.ts
@@ -0,0 +1,106 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Component, ClassAttributes } from "react";
+
+const formattedSeconds = (sec: number) => Math.floor(sec / 60) + ':' + ('0' + sec % 60).slice(-2);
+
+interface StopwatchProps extends ClassAttributes<Stopwatch> {
+  initialSeconds: number;
+}
+class Stopwatch extends Component<StopwatchProps, any> {
+  incrementer: any
+  laps: any[]
+
+  constructor(props: StopwatchProps) {
+    super(props);
+    this.state = {
+      secondsElapsed: props.initialSeconds,
+      lastClearedIncrementer: null,
+      laps: [],    // move laps within component state
+    }
+
+    // bind event handlers
+    this.handleStartClick = this.handleStartClick.bind(this);
+    this.handleStopClick = this.handleStopClick.bind(this);
+    this.handleLabClick = this.handleLabClick.bind(this);
+    this.handleResetClick = this.handleResetClick.bind(this);
+  }
+
+  handleStartClick() {
+    this.incrementer = setInterval(() =>
+      this.setState({
+        secondsElapsed: this.state.secondsElapsed + 1,
+      }), 1000);
+  }
+
+  handleStopClick() {
+    clearInterval(this.incrementer);
+    this.setState({
+      lastClearedIncrementer: this.incrementer,
+    });
+  }
+
+  handleResetClick() {
+    clearInterval(this.incrementer);
+      this.setState({
+        secondsElapsed: 0,
+        laps: [], // update laps as part of state
+      });
+  }
+
+  handleLapClick() {    // fix spelling for 'ap'
+    
+    // instead of this...
+    // this.laps = this.laps.concat([this.state.secondsElapsed]);
+    // this.forceUpdate();
+
+    // ... we should update laps as part of component state
+    this.setState({
+      lap: this.state.laps.concat([this.state.secondsElapsed]),
+    });
+  }
+
+  handleDeleteClick(index: number) {
+    return () => this.laps.splice(index, 1);
+  }
+
+  render() {
+    const {
+      secondsElapsed,
+      lastClearedIncrementer,
+    } = this.state;
+
+    const Lap = (props: { index: number, lap: number, onDelete: () => {} }) => (
+      <div key={props.index} className="stopwatch-lap">
+        <strong>{props.index}</strong>/ {formattedSeconds(props.lap)}
+        <button onClick={props.onDelete} > X </button>
+      </div>
+    );
+  
+    return (
+      <div className="stopwatch">
+        <h1 className="stopwatch-timer">{formattedSeconds(secondsElapsed)}</h1>
+        {(secondsElapsed === 0 || this.incrementer === lastClearedIncrementer
+          ? <button type="button" className="start-btn" onClick={this.handleStartClick}>start</button>
+          : <button type="button" className="stop-btn"  onClick={this.handleStopClick}>stop</button>
+        )}
+        {(secondsElapsed !== 0 && this.incrementer !== lastClearedIncrementer
+          ? <button type="button" onClick={this.handleLabClick}>lap</button>
+          : null
+        )}
+        {(secondsElapsed !== 0 && this.incrementer === lastClearedIncrementer
+          ? <button type="button" onClick={this.handleResetClick}>reset</button>
+          : null
+        )}
+        <div className="stopwatch-laps">
+          { this.laps && this.laps.map((lap, i) => <Lap index={i+1} lap={lap} onDelete={this.handleDeleteClick(i)} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Stopwatch initialSeconds={0} />,
+  document.getElementById("content"),
+);
